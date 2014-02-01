e31b677e20bcacf6c9b2775c9740939af35a13a7
Allan MacGregor <amacgregor@allanmacgregor.com>
2014-02-01 13:56:19 -0500

    Partial refactor of the related products block

--- app/code/community/Magehack/Predictions/Block/Catalog/Product/List/Related.php
+++ app/code/community/Magehack/Predictions/Block/Catalog/Product/List/Related.php
@@ -4,13 +4,25 @@ class Magehack_Predictions_Block_Catalog_Product_List_Related extends Mage_Catal
 {
     protected function _prepareData()
     {
-        if ( ! Mage::helper('predictions')->canOverrideRelatedProducts())
+        // Default back to core functionality
+        if (!Mage::helper('predictions')->canOverrideRelatedProducts()){
             return parent::_prepareData();
-
-        $product = Mage::registry('product');
-        /* @var $product Mage_Catalog_Model_Product */
+        }
 
         /* Retrieve related product ids here. */
+        $predictionHelper   = Mage::helper('predictions');
+        $cookieId           = $predictionHelper->getCurrentUserUniqueId();
+
+        $recommendationCollection = Mage::getModel('predictions/recommendations')
+            ->getCollection()
+            ->addFieldToFilter('cookie_id', array('eq' => $cookieId));
+
+        if (Mage::getSingleton('customer/session')->isLoggedIn()) {
+            $customerData = Mage::getSingleton('customer/session')->getCustomer();
+            $recommendationCollection->addFieldToFilter('customer_id', array('eq' => $customerData->getId()));
+        }
+
+
         $predictionioIds = array(16, 17, 18, 19, 20, 21, 22, 23, 30, 31, 32);
 
         // [review] - Review the logic for loading the product collection for this block
@@ -25,7 +37,8 @@ class Magehack_Predictions_Block_Catalog_Product_List_Related extends Mage_Catal
             );
             $this->_addProductAttributesAndPrices($this->_itemCollection);
         }
-//        Mage::getSingleton('catalog/product_status')->addSaleableFilterToCollection($this->_itemCollection);
+
+
         Mage::getSingleton('catalog/product_visibility')->addVisibleInCatalogFilterToCollection($this->_itemCollection);
 
         $this->_itemCollection->load();
