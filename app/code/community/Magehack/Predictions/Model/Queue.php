9a458be624ad856ad613509a9a62b17dc6c04f2d
Allan MacGregor <amacgregor@allanmacgregor.com>
2014-02-01 04:13:13 -0500

    Clean up Queue code/community/Magehack/Predictions/Model/Observer.php Update Observer.php

--- app/code/community/Magehack/Predictions/Model/Queue.php
+++ app/code/community/Magehack/Predictions/Model/Queue.php
@@ -1,8 +1,8 @@
 <?php
-class Magehack_Predictions_Model_Queue extends Mage_Core_Model_Abstract {
-
+class Magehack_Predictions_Model_Queue extends Mage_Core_Model_Abstract
+{
     protected function _construct()
     {
         $this->_init('predictions/queue', 'queue_id');
-    }}
-?>
+    }
+}
