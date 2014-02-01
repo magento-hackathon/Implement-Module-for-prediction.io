c852c12bdc8744e2dd641a18e2c1526a5fb94531
Matt Dunbar <matt@mattdsworld.com>
2014-02-01 08:34:07 -0500

    Updated predictions engine

---
+++ app/code/community/Magehack/Predictions/Helper/Adapters/Myrrix.php
@@ -0,0 +1,65 @@
+<?php
+class Magehack_Predictions_Helper_Adapters_Myrrix implements Magehack_Predictions_Helper_Adapters_Interface {
+    protected $sdk;
+
+    public function __construct() {
+        require_once(Mage::getBaseDir('lib') . '/Predictions/MyrrixSDK.php');
+        $this->sdk = new MyrrixSDK(Mage::helper('predictions')->getMyrrixBaseUri());
+    }
+
+    /**
+     * Notify Myrrix of the product view.
+     *
+     * @param int Magento customer id or cookie id
+     * @param int Magento product id
+     * @return null
+     */
+    public function view($user_identifier, $product_id) {
+        $this->sdk->ingest($user_identifier . ',' . $product_id . ',' . Mage::helper('predictions')->getMyrrixViewPoints());
+    }
+
+    /**
+     * Notify Myrrix of the add to cart.
+     *
+     * @param int Magento customer id or cookie id
+     * @param int Magento product id
+     * @return null
+     */
+    public function addToCart($user_identifier, $product_id) {
+        $this->sdk->ingest($user_identifier . ',' . $product_id . ',' . Mage::helper('predictions')->getMyrrixAddToCartPoints());
+    }
+
+    /**
+     * Notify Myrrix of the add to wishlist.
+     *
+     * @param int Magento customer id or cookie id
+     * @param int Magento product id
+     * @return null
+     */
+    public function addToWishlist($user_identifier, $product_id) {
+        $this->sdk->ingest($user_identifier . ',' . $product_id . ',' . Mage::helper('predictions')->getMyrrixAddToWishlistPoints());
+    }
+
+    /**
+     * Notify Myrrix of ordered product.
+     *
+     * @param int Magento customer id or cookie id
+     * @param int Magento product id
+     * @return null
+     */
+    public function order($user_identifier, $product_id) {
+        $this->sdk->ingest($user_identifier . ',' . $product_id . ',' . Mage::helper('predictions')->getMyrrixOrderPoints());
+    }
+
+    /**
+     * Notify Myrrix of a return.
+     *
+     * @TODO: This method is never called, should add EE support.
+     * @TODO: Push this data to Myrrix
+     *
+     * @param int Magento customer id or cookie id.
+     * @param int Magento product id.
+     * @return null
+     */
+    public function productReturn($user_identifier, $product_id) {}
+}
--- app/code/community/Magehack/Predictions/Helper/Adapters/Predictionio.php
+++ app/code/community/Magehack/Predictions/Helper/Adapters/Predictionio.php
@@ -4,7 +4,7 @@ class Magehack_Predictions_Helper_Adapters_Predictionio implements Magehack_Pred
 
     public function __construct() {
         require_once(Mage::getBaseDir('lib') . '/Predictions/PredictionIOSDK.php');
-        $this->sdk = new PredictionsIOSDK(Mage::helper('predictions')->getPredictionIOBaseUri(), Mage::helper('predictions')->getPredictionIOApiKey());
+        $this->sdk = new PredictionIOSDK(Mage::helper('predictions')->getPredictionIOBaseUri(), Mage::helper('predictions')->getPredictionIOApiKey());
     }
 
     /**
@@ -41,7 +41,7 @@ class Magehack_Predictions_Helper_Adapters_Predictionio implements Magehack_Pred
     }
 
     /**
-     * Process the array of orders and notify PredictionIO about each.
+     * Notify PredictionIO of ordered product.
      *
      * @param int Magento customer id or cookie id
      * @param int Magento product id
