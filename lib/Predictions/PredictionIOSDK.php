841c2a66a9bb9d23203db30a19eae33e3a0dc8af
Matt Dunbar <matt@buildrx.com>
2014-02-01 01:16:48 -0500

    Added predictionio sdk and adapter

---
+++ lib/Predictions/PredictionIOSDK.php
@@ -0,0 +1,143 @@
+<?php
+class PredictionIOSDK {
+    protected $serverLocation, $apiKey;
+
+    /**
+     * Save the server location and api key for requests.
+     *
+     */
+    public function __construct($serverLocation, $apiKey) {
+        $this->serverLocation = $serverLocation;
+        $this->apiKey = $apiKey;
+    }
+
+    /**
+     * Create user in PredictionIO.
+     *
+     * @param string Unique identifier for the user being created
+     * @return null
+     */
+    public function addUser($user_id) {
+        $this->apiCall('users.json', 'POST', array('pio_uid' => $uid));
+    }
+
+    /**
+     * Create item in PredictionIO.
+     *
+     * @param string Unique identifier for the item being created
+     * @param string Type of item
+     * @return null
+     */
+    public function addItem($item_id, $item_type) {
+        $this->apiCall('items.json', 'POST', array('pio_iid' => $item_id, 'pio_itypes' => $item_type));
+    }
+
+    /**
+     * Delete item from PredictionIO.
+     *
+     * @param string Unique identifier for the item being deleted
+     * @return null
+     */
+    public function deleteItem($item_id) {
+        $this->apiCall('items/' . $item_id . '.json', 'DELETE');
+    }
+
+    /**
+     * Record a like action.
+     *
+     * @param string Unique identifier for the user
+     * @param string Unique identifier for the item
+     * @return null
+     */
+    public function likeItem($user_id, $item_id) {
+        $this->itemAction($user_id, $item_id, 'like');
+    }
+
+    /**
+     * Record a dislike action.
+     *
+     * @param string Unique identifier for the user
+     * @param string Unique identifier for the item
+     * @return null
+     */
+    public function dislikeItem($user_id, $item_id) {
+        $this->itemAction($user_id, $item_id, 'dislike');
+    }
+
+    /**
+     * Record a view action.
+     *
+     * @param string Unique identifier for the user
+     * @param string Unique identifier for the item
+     * @return null
+     */
+    public function viewItem($user_id, $item_id) {
+        $this->itemAction($user_id, $item_id, 'view');
+    }
+
+    /**
+     * Record a conversion action.
+     *
+     * @param string Unique identifier for the user
+     * @param string Unique identifier for the item
+     * @return null
+     */
+    public function conversionItem($user_id, $item_id) {
+        $this->itemAction($user_id, $item_id, 'conversion');
+    }
+
+
+    /**
+     * Record an item action.
+     *
+     * @param string Unique identifier for the user
+     * @param string Unique identifier for the item
+     * @param string Internally recognized name of the action being taken
+     * @return null
+     */
+    protected function itemAction($user_id, $item_id, $action) {
+        $this->apiCall('actions/u2i.json', 'POST', array('pio_uid' => $user_id, 'pio_iid' => $item_id, 'pio_action' => $action));
+    }
+
+    /**
+     * Create an api request and inject the app token.
+     *
+     * @param string Path to the rest method being called
+     * @param string Request method: GET, POST, PUT or DELETE
+     * @param array An array of parameters
+     * @return null
+     * @throws Exception Invalid or failed requests throw exceptions.
+     */
+    protected function apiCall($path, $requestMethod, $params = array()) {
+        $requestMethod = strtoupper($requestMethod);
+        switch($requestMethod) {
+            case 'GET':
+                $setParamMethod = 'setParameterGet';
+                break;
+            case 'POST':
+            case 'PUT':
+            case 'DELETE':
+                $setParamMethod = 'setParameterPost';
+                break;
+            default:
+                throw new Exception('Invalid request method');
+        }
+
+        $client = new Varien_Http_Client($this->serverLocation . $path);
+        $client->setMethod($requestMethod);
+
+        call_user_func(array($client, $setParamMethod), 'pio_appkey', $this->apiKey);
+
+        foreach($params as $paramKey => $paramValue) {
+            call_user_func(array($client, $setParamMethod), $paramKey, $paramValue);
+        }
+
+        $response = $client->request();
+        if ($response->isSuccessful()) {
+            return json_decode($response->getBody());
+        } else {
+            throw new Exception('Request failed');
+        }
+
+    }
+}
