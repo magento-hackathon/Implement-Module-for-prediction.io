6e9c9cea09fa77f747935b47c897b213811b6951
Matt Dunbar <matt@buildrx.com>
2014-02-01 02:46:16 -0500

    Simplified source for dropdown (engine selection)

--- app/code/community/Magehack/Predictions/Model/Adminhtml/System/Config/Source/Predictions/Engine.php
+++ app/code/community/Magehack/Predictions/Model/Adminhtml/System/Config/Source/Predictions/Engine.php
@@ -3,21 +3,18 @@
 class Magehack_Predictions_Model_Adminhtml_System_Config_Source_Predictions_Engine
 {
 
-    public function toArray()
-    {
-        return array(
-            Magehack_Predictions_Model_Engine::ENGINE_PREDICTIONIO => Mage::helper('predictions')->__('PredictionIO'),
-            Magehack_Predictions_Model_Engine::ENGINE_MYRRIX => Mage::helper('predictions')->__('Myrrix'),
-        );
-    }
-
     public function toOptionArray()
     {
-        $retval = array();
-        foreach ($this->toArray() as $value => $label)
-            $retval[] = array('value' => $value, 'label' => $label);
-
-        return $retval;
+        return array(
+            array(
+                'label' => Mage::helper('predictions')->__('PredictionIO'),
+                'value' => Magehack_Predictions_Model_Engine::ENGINE_PREDICTIONIO
+            ),
+            array(
+                'label' => Mage::helper('predictions')->__('Myrrix'),
+                'value' => Magehack_Predictions_Model_Engine::ENGINE_MYRRIX
+            )
+        );
     }
 
 }
